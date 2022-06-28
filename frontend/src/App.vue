d767b5b1755b5a60779fde82bcfa7cf614fa6860
mljavadevs <mljavadevs@gmail.com>
2022-06-28 15:55:02 +0530

    limit changed

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -11,17 +11,18 @@ export default {
   },
   methods: {
     searchAPI(e) {
-      this.searchValue = this.inputValue.trim()
-      this.response = []
-      this.loading = true
+      this.searchValue = this.inputValue.trim();
+      this.response = [];
+      this.loading = true;
       axios
         .get(
-          `${import.meta.env.VITE_SERVER
-          }/symptoms-search?page=1&size=50&search=` + this.searchValue
+          `${
+            import.meta.env.VITE_SERVER
+          }/symptoms-search?page=1&size=80&search=` + this.searchValue
         )
         .then((res) => {
           this.response = res.data.data;
-          this.loading = false
+          this.loading = false;
         })
         .catch((e) => {
           console.log(e);
@@ -31,15 +32,15 @@ export default {
       const list = str.toLowerCase().split(this.searchValue.toLowerCase());
       const output = [];
       var counter = 0;
-      list.forEach(item => {
+      list.forEach((item) => {
         const itemLen = item.length;
         output.push(str.substr(counter, itemLen));
         counter += itemLen;
-        output.push(str.substr(counter, this.searchValue.length))
+        output.push(str.substr(counter, this.searchValue.length));
         counter += this.searchValue.length;
       });
       return output;
-    }
+    },
   },
 };
 </script>
@@ -62,14 +63,25 @@ export default {
 </style>
 
 <template>
-  <input v-model="inputValue" @change="searchAPI($event)" placeholder="Type here..." class="textField" />
+  <input
+    v-model="inputValue"
+    @change="searchAPI($event)"
+    placeholder="Type here..."
+    class="textField"
+  />
   <button @click="searchAPI($event)" class="searchButton">Search</button>
   <div v-if="loading">Loading...</div>
   <div v-else>
     <ol id="highlightString">
       <li v-for="string in response" :key="string">
-        <p v-for="item in getHighlightString(string)" :key="item" class="inlinePrint">
-          <span v-if="item.toLowerCase() === this.searchValue.toLowerCase()"><b>{{ item }}</b></span>
+        <p
+          v-for="item in getHighlightString(string)"
+          :key="item"
+          class="inlinePrint"
+        >
+          <span v-if="item.toLowerCase() === this.searchValue.toLowerCase()"
+            ><b>{{ item }}</b></span
+          >
           <span v-else>{{ item }}</span>
         </p>
       </li>
